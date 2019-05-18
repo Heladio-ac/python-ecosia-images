0ae54ab268d84d87b15456a87da4d0ce4b8f894e
Heladio <heladio.ac@gmail.com>
2019-05-17 21:33:42 -0500

    add browser capabilities

--- ecosia.py
+++ ecosia.py
@@ -1,16 +1,23 @@
+from selenium import webdriver
 import requests
-from bs4 import BeautifulSoup
 
-def search(keywords):
-    payload = {
-        "q": keywords
-    }
+class searcher:
+    def __init__(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--no-sandbox')
+        options.add_argument('--headless')
+    
+        self.driver = webdriver.Chrome(executable_path='./chromedriver', chrome_options=options)
 
-    url = "https://www.ecosia.org/images"
 
-    html = requests.get(url, payload)
-    parsed_html = BeautifulSoup(html.text, features='html.parser')
-    for link in parsed_html.find_all("a", class_="image-result js-image-result js-infinite-scroll-item"):
-        print(link.attrs['href'])
+    def search(self, keywords):
+        self.driver.get("https://www.ecosia.org/images?q=<%s>" % keywords)
+        try:
+            elements = self.driver.find_elements_by_class_name('image-result')
+            for element in elements:
+                print(element.get_property("href"))
+        except:
+            print('Was not able to find an element with that name.')
 
-search("perritos")
+x = searcher()
+x.search('perritos')
